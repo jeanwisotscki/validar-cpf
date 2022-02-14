1e4d8312e4bc4a8ce612f2b879d86cfff449350f
Jean Wisotscki <jean1wisotscki@gmail.com>
2022-02-14 14:44:19 -0300

    script e módulos criados

---
+++ js/modules/validar-cpf.js
@@ -0,0 +1,5 @@
+export default class ValidarCPF {
+  constructor(element) {
+    this.element = element;
+  }
+}
---
+++ js/script.js
@@ -0,0 +1,3 @@
+import ValidarCPF from "./modules/validar-cpf.js";
+
+const validarCpf = new ValidarCPF();
